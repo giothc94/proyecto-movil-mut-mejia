41eaa23e38312a494319f13e487c4fa94dd7065d
Gabriel Costta <gio666nb@gmail.com>
2019-04-26 01:58:25 -0500

    Modificando Tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,33 @@
+import { ModalTurismoComponent } from './../components/modal-turismo/modal-turismo.component';
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+
+
+export class Tab2Page {
+  
+  constructor(public modalController: ModalController){
+
+  }
+  
+  mostrarDetalleTurismo(uid){
+    this.presentModal(uid)
+  }
+
+  async presentModal(uid) {
+    const modal = await this.modalController.create({
+      component: ModalTurismoComponent,
+      componentProps:
+        { 
+          uid:uid
+        }
+    });
+    return await modal.present();
+  }
+}
