6b6198695b8a9fcfb014d50abab6e04020d1ccb3
giothcode <38662171+giothc94@users.noreply.github.com>
2019-04-26 03:21:47 -0500

    Corrigiendo error en rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'modal-paradas', loadChildren: './pages/modal-paradas/modal-paradas.module#ModalParadasPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
 ];
 @NgModule({
   imports: [
